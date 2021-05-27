e3752abebabbc823067f4838d966cbbffb08cbc5
nipan09 <pandey.nishant489@gmail.com>
2021-05-27 15:34:57 +0530

    init design with change click function

---
+++ navbar/src/components/navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { MenuItems } from './MenuItems'
+import { useState } from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    const [click, setClick] = useState({
+        clicked: false,
+    })
+
+    const handleClick = () => {
+        setClick({clicked: !click.clicked})
+    }
+
+    return (
+        <nav className="NavBarItems">
+            <h1 className="navbar-logo">React <i className="fab fa-react"></i></h1>
+            <div className="menu-icon" onClick = {handleClick}>
+                <i className={click.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+            </div>
+            <ul>
+                { MenuItems.map((item) => {
+                    return(                 // map always return
+                        <li>
+                            <a className={ item.cName } href={ item.url }>
+                                {item.title}
+                            </a>
+                        </li>
+                        )
+                })}
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
